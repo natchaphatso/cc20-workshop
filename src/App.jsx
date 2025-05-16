e451ff8eb9cca3adfa384f1f02e7e1637b038414
natchaphatso <natchaphatso@gmail.com>
2025-05-16 09:21:24 +0700

    starter with daisyui

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="app">
+      <h1 className="text-2xl">App</h1>
+      <button className="btn btn-primary">Okie</button>
+    </div>
+  );
+}
+
+export default App;
